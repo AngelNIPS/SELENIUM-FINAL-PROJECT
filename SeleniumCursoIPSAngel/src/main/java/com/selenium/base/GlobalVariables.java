fb451ba2f1a3fe627cccb26498f465d02e86ad68
Angel Nunez <Angel.nunez@ipointsystems.com>
2022-08-25 17:17:02 -0500

    Automation Report Added

--- SeleniumCursoIPSAngel/src/main/java/com/selenium/base/GlobalVariables.java
+++ SeleniumCursoIPSAngel/src/main/java/com/selenium/base/GlobalVariables.java
@@ -4,6 +4,10 @@ public class GlobalVariables {
 	
 	public static final String QA_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
 	public static final String QA_EXCERCISE = "https://computer-database.gatling.io/computers";
-	public static final String PATH_JSON_DATA = "./src/test/testdata/json/";
+	public static final String PATH_JSON_DATA = "./src/test/resources/testdata/json/";
 	public static final String PATH_SCREENSHOTS = System.getProperty("user.dir")+"/test-output/screenshots/";
+	public static final String EXTENT_QA_URL = "QA URL";
+	public static final String USER_DIR = "user.dir";
+	public static final String REPORT_PATH ="/test-output/Final_Test_Report.html";
+	public static final String CONFIG_EXTENT ="\\extent-config.xml";
 }
--- SeleniumCursoIPSAngel/src/test/java/com/finalproject/FinalProject.java
+++ SeleniumCursoIPSAngel/src/test/java/com/finalproject/FinalProject.java
@@ -1,11 +1,18 @@
 package com.finalproject;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import com.selenium.base.Base;
 import com.selenium.base.GlobalVariables;
 import com.selenium.poc.Dashboard;
@@ -21,6 +28,10 @@ public class FinalProject {
 	String password;
 	String newusername;
 	String newpassword;
+	ExtentReports extent;
+	ExtentTest logger;
+
+	private String className = this.getClass().getSimpleName();
 	
 	@BeforeTest
 	public void beforeTest() {
@@ -32,19 +43,42 @@ public class FinalProject {
 		password = base.getJSONValue("tcfinal", "password");
 		newusername = base.getJSONValue("tcfinal", "newuser");
 		newpassword = base.getJSONValue("tcfinal", "newpassword");
+		
+		// Set up Extend reports
+		extent = new ExtentReports(System.getProperty(GlobalVariables.USER_DIR) + GlobalVariables.REPORT_PATH, true);
+		extent.addSystemInfo(GlobalVariables.EXTENT_QA_URL, GlobalVariables.QA_URL);
+		extent.loadConfig(new File(System.getProperty(GlobalVariables.USER_DIR) + GlobalVariables.CONFIG_EXTENT));
 	}
 	
 	@Test
 	public void tcfinal() {
+		logger = extent.startTest(className + " - " + new Object() {
+		}.getClass().getEnclosingMethod().getName());
+		
 		base.launchBrowser(GlobalVariables.QA_URL);
 		Assert.assertTrue(loginPage.login(username, password));
 		Assert.assertTrue(dashboard.navigateToAdmin());
 		Assert.assertTrue(dashboard.addNewUser(newusername, newpassword));
 		Assert.assertTrue(dashboard.deleteUser(newusername));
+		
+		logger.log(LogStatus.PASS, "Validation " + className + " - " + new Object() {
+		}.getClass().getEnclosingMethod().getName() + " was successfully");	
+	}
+	
+	@AfterMethod
+	public void getResult(ITestResult result) {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			logger.log(LogStatus.FAIL, logger.addScreenCapture(base.takeScreenshot("TCFINAL - ERROR")));
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			logger.log(LogStatus.SKIP, "Test Case Skipped is " + result.getName());
+		}
+		extent.endTest(logger);
 	}
 	
 	@AfterTest
 	public void afterTest() {
+		extent.flush();
+		extent.close();
 		base.closeBrowser();
 	}
 
